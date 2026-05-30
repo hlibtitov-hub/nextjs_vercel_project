8e9e7a0ef981d7038efe53b863773928a29aa466
Hlib Tytov <hlibtytov@MacBook-Air-Hlib.local>
2026-05-30 12:17:10 +0200

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>My First Next.js App</h1>
+      <p>This app is deployed with Vercel.</p>
+      <button>Get Started</button>
+    </main>
+  );
+}
